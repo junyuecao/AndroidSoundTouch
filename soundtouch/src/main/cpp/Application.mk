cab282b4fb2eeb21b74109ee124a38216c1e717c
junyuecao <junyuecao@gmail.com>
2017-09-30 00:06:50 +0800

    Add x86 support

--- soundtouch/src/main/cpp/Application.mk
+++ soundtouch/src/main/cpp/Application.mk
@@ -6,5 +6,5 @@
 APP_ABI := all #armeabi-v7a armeabi
 APP_OPTIM := release
 APP_STL := stlport_static
-APP_CPPFLAGS := -fexceptions # -D SOUNDTOUCH_DISABLE_X86_OPTIMIZATIONS
+APP_CPPFLAGS := -fexceptions -D SOUNDTOUCH_DISABLE_X86_OPTIMIZATIONS
 
